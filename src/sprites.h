3685202e842566b75ebfe81decdc395b92f83d47
Max Carvajal <jcarvajal288@gmail.com>
2026-07-11 13:07:07 -0500

    Display stationary head tile using the background

--- src/sprites.h
+++ src/sprites.h
@@ -5,6 +5,22 @@
 
 #define NUM_SNAKE_TILES 15
 
+#define BLANK_TILE 0x00
+#define HEAD_TILE_N 0x01
+#define HEAD_TILE_W 0x02
+#define HEAD_TILE_E 0x03
+#define HEAD_TILE_S 0x04
+#define BODY_VERTICAL_TILE 0x05
+#define BODY_HORIZONTAL_TILE 0x06
+#define BODY_BENT_TILE_NE 0x07
+#define BODY_BENT_TILE_SE 0x08
+#define BODY_BENT_TILE_SW 0x09
+#define BODY_BENT_TILE_NW 0x0a
+#define TAIL_TILE_N 0x0b
+#define TAIL_TILE_W 0x0c
+#define TAIL_TILE_E 0x0d
+#define TAIL_TILE_S 0x0e
+
 extern const uint8_t snake_tiles[0x10 * NUM_SNAKE_TILES];
 
 #endif
