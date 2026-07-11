089cf10d0112480c80c9bc20d74932e1a6a86cc0
Max Carvajal <jcarvajal288@gmail.com>
2026-07-10 21:10:46 -0500

    Add other snake sprites

---
+++ sprites/body_straight_sprite.h
@@ -0,0 +1,8 @@
+#ifndef METASPRITE_body_straight_sprite_H
+#define METASPRITE_body_straight_sprite_H
+
+#include <stdint.h>
+
+extern const uint8_t BodyStraightSprite[16];
+
+#endif
---
+++ sprites/tail_sprite.h
@@ -0,0 +1,8 @@
+#ifndef METASPRITE_tail_sprite_H
+#define METASPRITE_tail_sprite_H
+
+#include <stdint.h>
+
+extern const uint8_t TailSprite[16];
+
+#endif
